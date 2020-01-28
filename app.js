26fcb7761e6e7c6fc661ae7ff7812c91b836cd42
Carlos Tovar <carlos.tovar@ariwonto.com>
2020-01-28 18:05:57 -0500

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const SerialPort = require("serialport");
+const SerialPortParser = require("@serialport/parser-readline");
+const GPS = require("gps");
+const Request = require("request-promise");
+
+const port = new SerialPort("/dev/ttyS0", { baudRate: 9600 });
+const gps = new GPS();
+
+//const APP_ID = "HERE_APP_ID";
+//const APP_CODE = "HERE_APP_CODE";
+
+const parser = port.pipe(new SerialPortParser());
+
+function getAddressInformation(latitude, longitude) {}
+
+gps.on("data", async data => {});
+parser.on("data", data => {});
