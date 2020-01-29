b039bc7cc99a475d57b5f06da8eee4e36df73a2b
Carlos Tovar <carlos.tovar@ariwonto.com>
2020-01-28 19:38:40 -0500

    fix

--- app.js
+++ app.js
@@ -11,14 +11,36 @@ const gps = new GPS();
 
 const parser = port.pipe(new SerialPortParser());
 
-//function getAddressInformation(latitude, longitude) {}
+function getAddressInformation(latitude, longitude) {
+    let address ={};
+    return Request({
+        "uri": "https://reverse.geocoder.api.here.com/6.2/reversegeocode.json",
+        "qs": {
+            "app_id": "4GqSZVAsHbtzpGMsSjcp",
+            "app_code": "JF5Cc3uvR-Aabh5-RBKD8VtSkKLhwlwQF4SLFQ43n0A",
+            "mode": "retrieveAddress",
+            "prox": latitude + "," + longitude
+        },
+        "json": true
+    }).then(result => {
+        if(result-Response.View.length > 0 && result.Response.View[0].Result.length > 0) {
+            address = result.Response.View[0].Result[0].Location.Address;
+        }
+        return address;
+    });
+}
 
 //gps.on("data", async data => {});
 
 gps.on("data", data => {
     if(data.type == "GGA") {
         if(data.quality != null){
-            console.log(data);
+            try{
+                let address = await getAddress(data.lat.toFixed(2), data.lon.toFixed(2));
+                console.log(address);
+            } catch (e) {
+                console.log(e);
+            }
         }
     }
 });
